af8c6b6dc30ed26e5088b0de2400eea7a4986c24
ga7821400-prog <ga7821400@gmail.com>
2025-12-13 06:00:27 +0400

    server.js

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir todos los archivos estáticos (CSS, JS, imágenes)
 app.use(express.static(__dirname));
 
-// Servir el index.html en la raíz
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Evitar 404 en cualquier otra ruta
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
